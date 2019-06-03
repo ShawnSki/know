467eb2b879509e8c5aba3b51583be7fad62efd45
shawn's macbook <shawnscivally@shawn-ski.local>
2019-06-03 15:24:38 -0600

    create quiz frontend working

--- src/Components/CreateQuiz/CreateQuiz.js
+++ src/Components/CreateQuiz/CreateQuiz.js
@@ -1,16 +1,46 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class CreateQuiz extends Component {
+    constructor() {
+        super()
+        this.state = {
+            quiz_title: '',
+            quiz_intro: '',
+            quiz_bg_img: ''
+
+        }
+    }
+
+    handleInfoUpdate = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleAddQuiz = (e) => {
+        e.preventDefault();
+        const { quiz_title, quiz_intro, quiz_bg_img } = this.state;
+        axios.post('/api/quiz', { quiz_title, quiz_intro, quiz_bg_img })
+            .then((res) => {
+                console.log(res.data)
+                // need to do something with the response... add to the list of quizzes
+            })
+        e.target.quiz_title.value = ''
+        e.target.quiz_intro.value = ''
+        e.target.quiz_bg_img.value = ''
+    }
+
     render() {
         return (
             <div>
                 <h1>Create Quiz</h1>
                 <div>
-                    <form>
+                    <form onSubmit={this.handleAddQuiz}>
                         <h2>Create New</h2>
-                        <input type='text' name='title' placeholder='title' onChange={this.handleLoginInfoUpdate} />
-                        <input type='text' name='introduction' placeholder='introduction' onChange={this.handleLoginInfoUpdate} />
-                        <input type='text' name='backgroundImg' placeholder='background image url' onChange={this.handleLoginInfoUpdate} />
+                        <input type='text' name='quiz_title' placeholder='title' onChange={this.handleInfoUpdate} />
+                        <input type='text' name='quiz_intro' placeholder='introduction' onChange={this.handleInfoUpdate} />
+                        <input type='text' name='quiz_bg_img' placeholder='background image url' onChange={this.handleInfoUpdate} />
                         <button>Create Quiz</button> <br />
                     </form>
                 </div>
