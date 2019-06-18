92af3c299e3cce18c9570a74b88eb0835d0411a0
shawn's macbook <shawnscivally@shawn-ski.local>
2019-06-18 16:42:28 -0600

    added link to share

--- src/Components/QuizItem/QuizItem.js
+++ src/Components/QuizItem/QuizItem.js
@@ -28,7 +28,7 @@ class QuizItem extends Component {
                     <Link to={`/results/${this.props.quizObj.id}`}><button><img src='https://yellingyak.com/wp-content/uploads/2019/06/icon_results.png' alt='results' /></button></Link>
                     <Link to={`/quiz/${this.props.quizObj.id}`}><button><img src='https://yellingyak.com/wp-content/uploads/2019/06/icon_edit.png' alt='edit' /></button></Link>
                     <button onClick={this.handleDeleteQuiz}><img src='https://yellingyak.com/wp-content/uploads/2019/06/icon_delete.png' alt='delete' /></button>
-                    <button ><img src='https://yellingyak.com/wp-content/uploads/2019/06/icon_share.png' alt='share' /></button>
+                    <Link to={`/knowwie/${this.props.quizObj.id}`}><button ><img src='https://yellingyak.com/wp-content/uploads/2019/06/icon_share.png' alt='share' /></button></Link>
                 </div>
             </div>
         )
